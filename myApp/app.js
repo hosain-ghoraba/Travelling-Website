9452f4eae46421562241b2893de8cf46e91f119d
hosain-ghoraba <102357226+hosain-ghoraba@users.noreply.github.com>
2022-12-19 16:18:20 +0200

    first commit

---
+++ myApp/app.js
@@ -0,0 +1,48 @@
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// app.get('/pizza' , function(req,res){
+// res.render('pizzaPage' , {ppp:"pizza"});
+// });
+// app.post('/pizza' , function(req , res){
+//   var x = req.body.user;
+//   var y = req.body.pass;
+//   console.log(x);
+//   console.log(y);
+
+// });
+
+app.get('/' , function(req,res){
+  res.render('searchresults');
+});
+
+app.post('/' , function(req,res){ // must first check if creditials are in the database
+  res.render('home');
+});
+
+// app.get('/home',function(req,res){
+// res.render('/home');
+// });
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
