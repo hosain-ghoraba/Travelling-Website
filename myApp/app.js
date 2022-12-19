135ed689ad9cf8bf07946ae6dde4eeeffba831b1
hosain-ghoraba <102357226+hosain-ghoraba@users.noreply.github.com>
2022-12-19 22:13:44 +0200

    haha

--- myApp/app.js
+++ myApp/app.js
@@ -1,9 +1,8 @@
-
+// ---------------------------------------------------------------------------
 var express = require('express');
 var path = require('path');
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -11,37 +10,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ----------------------------------------------------------------------------------- the part above is fixed
+
+/* below is the pizza page code from vedio 1 , to get an idea of how 'get' and 'post' functions are done in the easiest scenario
+
+ app.get('/pizza' , function(req,res){
+ res.render('pizzaPage' , {ppp:"pizza"});
+ });
+
+ app.post('/pizza' , function(req , res){
+   var x = req.body.user;
+   var y = req.body.pass;
+   console.log(x);
+   console.log(y);
+
+ });
+ */
+
+ /*
+html pages classifications :-
+
+annapurna     : destination
+bali          : destination
+cities        : category
+hiking        : category
+home          : direct page after login page
+inca          : destination
+islands       : category
+login         : first page to be displayed
+paris         : destination
+registration  : shown if user clicked on 'I don't have an account ' in the login page
+rome          : destination
+santorini     : destination
+searchresults : shown after clicking on the 'search' button exsisting in all the pages in this project
+wanttogo      : shown if user clicked on  'want to go list' in the home page
+s
+
+*/
+
+// -------------------------------------------------------------------------------------------start of our code
 
 
-// app.get('/pizza' , function(req,res){
-// res.render('pizzaPage' , {ppp:"pizza"});
-// });
-// app.post('/pizza' , function(req , res){
-//   var x = req.body.user;
-//   var y = req.body.pass;
-//   console.log(x);
-//   console.log(y);
-
-// });
 
 app.get('/' , function(req,res){
   res.render('login');
 });
 
-app.post('/' , function(req,res){ // must first check if creditials are in the database
-  res.render('home');
+app.post('/search' , function(req,res){ // i think it must take a parameter (the text written in search bar)
+  res.render('searchresults');
 });
 
-// app.get('/home',function(req,res){
-// res.render('/home');
-// });
-
-// haha
-// batika
-// coco
-// haha
-// boo
-// gogo
 
 
 
